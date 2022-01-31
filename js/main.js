c7b6501a9792b3a44c0dfa3f47c52709dfe54ef1
Dog <98501863+dogdogdogs@users.noreply.github.com>
2022-01-31 15:28:30 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,23 @@
+"use strict";
+
+{
+  const btn = document.getElementById("btn");
+
+  btn.addEventListener("click", () => {
+    const results = ["大吉", "小吉", "末吉", "凶"];
+    const n = Math.floor(Math.random() * results.length);
+    btn.textContent = results[n];
+
+    // switch (n) {
+    //   case 0:
+    //     btn.textContent = "大吉";
+    //     break;
+    //   case 1:
+    //     btn.textContent = "小吉";
+    //     break;
+    //   case 2:
+    //     btn.textContent = "凶";
+    //     break;
+    // }
+  })
+}
